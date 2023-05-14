66b61cd79bf744a9a2b8f32edc7ef891c7adf5da
Gary McMeekin <garymcm@gmail.com>
2023-05-14 07:45:30 -0400

    moved declaration of rabbitmq url

--- src/message/sendMessage.js
+++ src/message/sendMessage.js
@@ -1,14 +1,12 @@
 import amqp from 'amqplib'
 import log4js from '../logging/index.js'
 const logger = log4js.getLogger('sendMessage')
-const rabbitUrl = `amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`
-
-logger.info('Connecting to rabbitmq', rabbitUrl)
-
-const connection = await amqp.connect(rabbitUrl)
-const channel = await connection.createChannel()
 
 export default async function sendMessage(queue, message) {
+  const rabbitUrl = `amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`
+  logger.info('Connecting to rabbitmq', rabbitUrl)
+  const connection = await amqp.connect(rabbitUrl)
+  const channel = await connection.createChannel()
   try {
     channel.assertQueue(queue, {
       durable: true,
