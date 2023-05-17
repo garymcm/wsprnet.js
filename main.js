5f40043cb38cd7bdcd5a27e752cf2cf2cb36c89b
Gary McMeekin <garymcm@gmail.com>
2023-05-17 07:12:16 -0400

    Added logging to boostrap

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+import * as dotenv from 'dotenv'
 import spotConsumer from './src/consumers/spot/index.js'
+import log4js from '../../logging/index.js'
+
+const logger = log4js.getLogger('main')
+
+logger.info('Starting up')
+dotenv.config()
+
+logger.info('RABBITMQ_HOST: %s', process.env.RABBITMQ_HOST)
+logger.info('RABBITMQ_PORT: %s', process.env.RABBITMQ_PORT)
+logger.info(
+  'Listening for spots on %s',
+  process.env.RABBITMQ_SPOT_QUEUE || 'spot'
+)
 
 spotConsumer()
