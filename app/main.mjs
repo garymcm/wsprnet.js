c6fd934f36f292918825ae41915824133f68a6c7
Gary McMeekin <garymcm@gmail.com>
2023-05-19 09:00:57 -0400

    Add debug middleware

--- app/main.mjs
+++ app/main.mjs
@@ -8,6 +8,11 @@ const port = 80
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(function (req, res, next) {
+  console.log('Time: %d', Date.now())
+  next()
+})
+
 app.post('/post', (req, res) => {
   const message = { ...req.body }
   //sendMessage('status', message)
