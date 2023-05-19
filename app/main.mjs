14d5bcd82bbbe2496beb3145d336ae94270599ad
Gary McMeekin <garymcm@gmail.com>
2023-05-19 09:22:56 -0400

    Await promise for message sent

--- app/main.mjs
+++ app/main.mjs
@@ -9,9 +9,9 @@ const port = 80
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post('/post', (req, res) => {
+app.post('/post', async (req, res) => {
   const message = { ...req.body }
-  sendMessage(process.env.RABBITMQ_SPOT_QUEUE, req.body)
+  await sendMessage(process.env.RABBITMQ_SPOT_QUEUE, req.body)
   console.log('Posted message sent', message)
   res.send('1 spot(s) added')
 })
