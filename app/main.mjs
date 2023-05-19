b656f4a9244a8d08404facba191b19804cc6d63e
Gary McMeekin <garymcm@gmail.com>
2023-05-19 09:19:59 -0400

    Added .env processing

--- app/main.mjs
+++ app/main.mjs
@@ -1,31 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-
-// import sendMessage from '../src/message/sendMessage'
+import sendMessage from '../src/message/sendMessage'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 const port = 80
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(function (req, res, next) {
-  console.log('Time: %d', Date.now())
-  next()
-})
-
 app.post('/post', (req, res) => {
   const message = { ...req.body }
-  //sendMessage('status', message)
-
-  console.log('Post', message)
-  res.send('1 spot(s) added')
-})
-
-app.get('/post', (req, res) => {
-  const message = { ...req.body }
-  //sendMessage('status', message)
-
-  console.log('Get', message)
+  sendMessage(process.env.RABBITMQ_SPOT_QUEUE, req.body)
+  console.log('Posted message sent', message)
   res.send('1 spot(s) added')
 })
 
