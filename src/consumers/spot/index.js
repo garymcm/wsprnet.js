15101f52bbc959e20f6a64c6f6cd5e15a8663bfe
Gary McMeekin <garymcm@gmail.com>
2023-05-20 08:12:45 -0400

    optimized status consumer

--- src/consumers/spot/index.js
+++ src/consumers/spot/index.js
@@ -4,40 +4,9 @@ import log4js from '../../logging/index.js'
 import Consumer from '../Consumer.js'
 
 export default async function spotConsumer() {
+  const logger = log4js.getLogger('starting spotConsumer')
   const spotConsumer = new Consumer('spotConsumer', post)
   await spotConsumer.start()
   await spotConsumer.consume()
-
-  // dotenv.config()
-
-  const logger = log4js.getLogger('spotConsumer')
-
-  // const queue = process.env.RABBITMQ_SPOT_QUEUE || 'spot'
-  // const rabbitUrl = `amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`
-
-  // logger.info('Connecting to rabbitmq %s queue %s', rabbitUrl, queue)
-  // const conn = await amqp.connect(rabbitUrl)
-  // const channel = await conn.createChannel()
-
-  // channel.assertQueue(queue, {
-  //   durable: true,
-  // })
-
-  // channel.consume(
-  //   queue,
-  //   async function (msg) {
-  //     if (msg.content) {
-  //       const spot = JSON.parse(msg.content.toString())
-  //       const count = await post(spot)
-
-  //       logger.trace('acking post', count)
-  //       channel.ack(msg)
-  //       logger.trace('post result', count)
-  //     }
-  //   },
-  //   {
-  //     noAck: false,
-  //   }
-  // )
   logger.info('spotConsumer started')
 }
--- src/consumers/status/index.js
+++ src/consumers/status/index.js
@@ -1,38 +1,12 @@
-import amqp from 'amqplib'
-import postStatus from '../../controller/status/index.js'
+import post from '../../controller/status/index.js'
 import log4js from '../../logging/index.js'
-import * as dotenv from 'dotenv'
+
+import Consumer from '../Consumer.js'
 
 export default async function statusConsumer() {
-  dotenv.config()
-
-  const logger = log4js.getLogger('statusConsumer')
-
-  const queue = process.env.RABBITMQ_SPOT_QUEUE || 'status'
-  const rabbitUrl = `amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`
-
-  logger.info('Connecting to rabbitmq %s queue %s', rabbitUrl, queue)
-  const conn = await amqp.connect(rabbitUrl)
-  const channel = await conn.createChannel()
-
-  channel.consume(
-    queue,
-    async function (msg) {
-      if (msg.content) {
-        const status = JSON.parse(msg.content.toString())
-        const result = await postStatus(status)
-        channel.ack(msg)
-        logger.trace('postStatus result', result)
-      }
-    },
-    {
-      noAck: false,
-    }
-  )
-
-  channel.on('error', function (err) {
-    logger.error('channel error', err)
-  })
-
+  const logger = log4js.getLogger('starting statusConsumer')
+  const statusConsumer = new Consumer('statusConsumer', post)
+  await statusConsumer.start()
+  await statusConsumer.consume()
   logger.info('statusConsumer started')
 }
