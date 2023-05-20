c014c0ea59e65fa6719f1a6e3dc2a1a4bec08cf0
Gary McMeekin <garymcm@gmail.com>
2023-05-20 16:58:43 -0400

    improved connection and errors

--- src/message/sendMessage.js
+++ src/message/sendMessage.js
@@ -5,17 +5,18 @@ const logger = log4js.getLogger('sendMessage')
 export default async function sendMessage(queue, message) {
   const rabbitUrl = `amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`
   logger.info('Connecting to rabbitmq', rabbitUrl)
+  let connection
   try {
-    const connection = await amqp.connect(rabbitUrl)
+    connection = await amqp.connect(rabbitUrl)
     const channel = await connection.createChannel()
     await channel.assertQueue(queue, {
       durable: true,
     })
-    logger.info('Sending message', message)
     channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)))
     await channel.close()
-    await connection.close()
   } catch (error) {
     logger.error('Error sending message', error)
+  } finally {
+    if (connection) await connection.close()
   }
 }
