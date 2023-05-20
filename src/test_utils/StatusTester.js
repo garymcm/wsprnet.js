8029a5eef8101272fb685de219c4ec79798b184a
Gary McMeekin <garymcm@gmail.com>
2023-05-20 09:25:43 -0400

    Fixed status test util

--- src/test_utils/StatusTester.js
+++ src/test_utils/StatusTester.js
@@ -6,15 +6,7 @@ const options = setEnvFromArgs()
 
 const logger = log4js.getLogger('StatusTester')
 
-const message = {
-  rcall: 'W1GJM',
-  rgrid: 'FN42di',
-  rqrg: '21.0969999',
-  tqrg: '21.0969999',
-  tpct: '5',
-  dbm: '37',
-  version: 'CWSL_DIGI 0.85',
-}
+const message = options.jsonString
 
 logger.info('Sending status message')
 await sendMessage('status', message)
